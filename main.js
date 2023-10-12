b10b78eea866304e4ac2f896d690bc8b826db1c2
mohamedchokri1 <chokrimohamed170@gmail.com>
2023-10-12 12:46:00 +0200

    hi

---
+++ main.js
@@ -0,0 +1,8 @@
+var passwordSign=document.getElementsById('passwordSign')
+var emailSign=document.getElementById('emailSign')
+var formSign=document.getElementsByClassId('check')
+
+// formSign.addEventListener('Login' , (e) =>{
+    
+//     e.preventDefault()
+// })
